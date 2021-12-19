275a51dbbd952fd01a59f0575a78d6813bf17e5f
Hoang Trung Kien <htkien@MacBook-Air-cua-Hoang.local>
2021-12-19 23:57:18 +0700

    End of section 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,33 +1,39 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-
+    [Authorize]
     public class UsersController : BaseApiController
     {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        private readonly DataContext _context;
-
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
         [HttpGet]
-        public async Task<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            return Ok(await _userRepository.GetMembersAsync());
+
         }
 
         [HttpGet]
-        [Route("{id}")]
-        public async Task<AppUser> GetUserById(int id)
+        [Route("{username}")]
+        public async Task<ActionResult<AppUser>> GetUserByName(string username)
         {
-            return await _context.Users.FindAsync(id);
+            return Ok(await _userRepository.GetMemberByUsernameAsync(username));
         }
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -6,9 +9,28 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-
         public Byte[] PasswordHash { get; set; }
         public Byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        public string KnownAs { get; set; }
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CalculateAge();
+        // }
 
     }
 }
