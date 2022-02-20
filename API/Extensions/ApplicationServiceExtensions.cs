68b46f8093c01b8184be3f85d660a5b6bcfcd59b
Hoang Trung Kien <htkien@MacBook-Air-cua-Hoang.local>
2022-02-21 00:00:35 +0700

    fix

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -52,7 +52,7 @@ namespace API.Extensions
                     var pgPass = pgUserPass.Split(":")[1];
                     var pgHost = pgHostPort.Split(":")[0];
                     var pgPort = pgHostPort.Split(":")[1];
-                    connStr = $"Server={pgHost}; Port={pgPort}; User Id={pgUser}; Password={pgPass}; Database={pgDb}";
+                    connStr = $"Server={pgHost}; Port={pgPort}; User Id={pgUser}; Password={pgPass}; Database={pgDb};SslMode=Disable";
                 }
 
                 // Whether the connection string came from the local development configuration file
